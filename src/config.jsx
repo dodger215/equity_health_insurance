50e7697021e59158d8330dbc8d54781b18a81f9f
dodger215 <kelvinenosdzah2@gmail.com>
2025-03-12 15:21:09 +0330

    inital

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 
 
-const API_BASE_URL = "http://127.0.0.1:8000"
+const API_BASE_URL = "https://equity-health-insurance-agent-api-3.onrender.com"
 
 export const API_ENDPOINTS = {
   api: `${API_BASE_URL}`
