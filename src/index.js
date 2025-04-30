2feaeaa2cb92fa1f1d0bff1c6ee5fc50240439e7
dodger215 <kelvinenosdzah2@gmail.com>
2025-04-30 05:58:31 +0330

    Development

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ root.render(
 reportWebVitals();
 
 // Service Worker Registration
-// if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then(registration => {
-//         console.log('SW registered:', registration);
-//       })
-//       .catch(error => {
-//         console.log('SW registration failed:', error);
-//       });
-//   });
-// }
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('SW registered:', registration);
+      })
+      .catch(error => {
+        console.log('SW registration failed:', error);
+      });
+  });
+}
